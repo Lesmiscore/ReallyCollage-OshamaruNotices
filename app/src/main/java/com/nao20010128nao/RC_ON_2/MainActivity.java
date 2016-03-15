4a95c1bb06ff642172d1da7b80a25a8cf6b76eb6
nao20010128nao <nao20010128@gmail.com>
2016-03-15 10:16:28 +0900

    色々

--- app/src/main/java/com/nao20010128nao/RC_ON_2/MainActivity.java
+++ app/src/main/java/com/nao20010128nao/RC_ON_2/MainActivity.java
@@ -2,13 +2,37 @@ package com.nao20010128nao.RC_ON_2;
 
 import android.app.*;
 import android.os.*;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity 
 {
+	EditText mes,title;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		title=(EditText)findViewById(R.id.texTitle);
+		mes=(EditText)findViewById(R.id.texMes);
+		findViewById(R.id.btnEdtMes).setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				title.setVisibility(View.GONE);
+				mes.setVisibility(View.VISIBLE);
+			}
+		});
+		findViewById(R.id.btnEdtTitle).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v){
+					title.setVisibility(View.VISIBLE);
+					mes.setVisibility(View.GONE);
+				}
+			});
+		findViewById(R.id.start).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v){
+					startActivity(new Intent(MainActivity.this,NoticeActivity.class));
+				}
+			});
+		
     }
 }
---
+++ app/src/main/java/com/nao20010128nao/RC_ON_2/NoticeActivity.java
@@ -0,0 +1,12 @@
+package com.nao20010128nao.RC_ON_2;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeActivity extends AppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+}
